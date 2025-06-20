1ebdafef0552cabda19c9f69150cdb40b5946887
Lopez Damian <dennyslopez2013@gmail.com>
2025-06-20 17:18:12 -0500

    [UPD] imports

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
   import Welcome from './components/pages/Welcome.vue';
   import Layout from './components/layouts/Layout.vue';
-  import Dashboard from './components/pages/Dashboard.vue';
-  import Wrokout from './components/pages/Wrokout.vue';
+  // import Dashboard from './components/pages/Dashboard.vue';
+  // import Wrokout from './components/pages/Wrokout.vue';
 
   const selectedDisplay = 1;
 
